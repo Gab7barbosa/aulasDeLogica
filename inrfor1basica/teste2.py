073b0cb833ac04b21c229c0aa043d797b4eaa67c
Gab7Barbosa <gabriel.silva1521@aluno.ce.gov.br>
2025-09-01 11:58:44 -0300

    novo comit

--- inrfor1basica/teste2.py
+++ inrfor1basica/teste2.py
@@ -1,4 +1,26 @@
-peso=float(input("me diga seu peso"))
-altura=float(input("me diga sua altura"))
-imc= peso/altura**2
-print("seu imc é:", imc)
+#peso=float(input("me diga seu peso"))
+#altura=float(input("me diga sua altura"))
+#imc= peso/altura**2
+#print("seu imc é:", imc)
+#cont=0
+#vogais=["a""e""i""o""u"]
+#for i in vogais:
+#    palavras=input("digite uma palavra")
+#    cont+=1
+
+#peso=float(input("me diga seu peso"))
+#altura=float(input("digite sua altura"))
+#imc=peso/altura**2
+#print(imc)
+#if imc == 24.5:
+#    print(imc, "pesso ideal")
+#elif imc < 24.5:
+#    print(imc, "abaixo do peso")
+#else:
+#    print("acima do peso")
+
+#numeros=int(input("digite um numero"))
+#if numeros %2 ==0:
+#    print("seu numero é par")
+#elif numeros %2 !=0:
+#    print("seu numero é impar")        
