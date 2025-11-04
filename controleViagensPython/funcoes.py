a6174fe1845aa5dc327fda70ea0a2d6094e6229f
Gab7barbosa <gabriel.silva1521@aluno.ce.gov.br>
2025-11-04 10:37:24 -0300

    Add files via upload

---
+++ controleViagensPython/funcoes.py
@@ -0,0 +1,15 @@
+from tabulate import tabulate
+listaViagems=[]
+
+def registrar_viagems(listaViagems):
+    nome_motora=input("digite o nome do seu motorista: ")
+    destino=input("digite seu destino: ")
+    distamcia=float(input("digite a distamcia da viagem: "))  
+    gasto=float(input("digite o valor gasto na viagem: ")) 
+    consumo=gasto/distamcia
+    listaViagems.append(registrar_viagems) 
+def exibir_viagems(listaViagems):
+    return(listaViagems)
+    print(listaViagems(tabulate))
+
+def 
---
+++ controleViagensPython/programa.py
@@ -0,0 +1,11 @@
+from funcoes import *
+from tabulate import tabulate
+while True:
+    print("1-registrar viagem")
+    print("2-exibir viagems")
+    print("3-buscar motorista")
+
+
+    op=input("digite uma opçao: ")
+
+    
